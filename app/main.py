ea73170fea91e301e13275109b237c2648a4e722
easwee <easwee@gmx.com>
2025-01-10 00:01:26 +0100

    Remove logging

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 import json
-from typing import Union
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Form, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-
 from app.settings import WORKFLOW_MAP
 from app.utils import run_workflow, urlx_for
 
@@ -25,17 +23,19 @@ async def get_index(request: Request):
     workflows = list(WORKFLOW_MAP.keys())
 
     return templates.TemplateResponse(
-        request=request, name="pages/index.html", context={"workflows": workflows}
+        request=request,
+        name="pages/index.html",
+        context={"workflows": workflows},
     )
 
 
-class InferPayload(BaseModel):
+class WorkflowPayload(BaseModel):
     workflow: str
     context: str
 
 
-@app.post("/infer/")
-async def infer(payload: InferPayload):
+@app.post("/workflow/")
+async def workflow(payload: WorkflowPayload):
     workflow = payload.workflow
     context_input = payload.context
 
@@ -43,8 +43,6 @@ async def infer(payload: InferPayload):
         context = json.loads(context_input)
     except json.JSONDecodeError:
         context = {"input": context_input}
-
-    print("HERE context", context)
 
     return StreamingResponse(
         run_workflow(workflow=workflow, context=context),
