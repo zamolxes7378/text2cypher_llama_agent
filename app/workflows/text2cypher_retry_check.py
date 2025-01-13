db6f81ec991a332f33d32a9b624dd69472b8f020
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2025-01-13 10:38:29 +0100

    Add information check workflow

---
+++ app/workflows/text2cypher_retry_check.py
@@ -0,0 +1,159 @@
+from llama_index.core import VectorStoreIndex
+from llama_index.core.schema import TextNode
+from llama_index.core.workflow import (
+    Context,
+    Event,
+    StartEvent,
+    StopEvent,
+    Workflow,
+    step,
+)
+
+from app.workflows.shared import (
+    SseEvent,
+    default_llm,
+    embed_model,
+    fewshot_examples,
+    graph_store,
+)
+from app.workflows.steps.naive_text2cypher import (
+    correct_cypher_step,
+    evaluate_database_output_step,
+    generate_cypher_step,
+    get_naive_final_answer_prompt,
+)
+
+
+class SummarizeEvent(Event):
+    question: str
+    cypher: str
+    context: str
+
+
+class ExecuteCypherEvent(Event):
+    question: str
+    cypher: str
+
+
+class CorrectCypherEvent(Event):
+    question: str
+    cypher: str
+    error: str
+
+
+class EvaluateEvent(Event):
+    question: str
+    cypher: str
+    context: str
+
+
+class NaiveText2CypherRetryCheckFlow(Workflow):
+    max_retries = 2
+
+    def __init__(self, llm=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # Call the parent init
+        self.llm = llm or default_llm  # Add child-specific logic
+
+        # Add fewshot in-memory vector db
+        few_shot_nodes = []
+        for example in fewshot_examples:
+            few_shot_nodes.append(
+                TextNode(
+                    text=f"{{'query':{example['query']}, 'question': {example['question']}))"
+                )
+            )
+        few_shot_index = VectorStoreIndex(few_shot_nodes, embed_model=embed_model)
+        self.few_shot_retriever = few_shot_index.as_retriever(similarity_top_k=5)
+
+    @step
+    async def generate_cypher(self, ctx: Context, ev: StartEvent) -> ExecuteCypherEvent:
+        # Init global vars
+        await ctx.set("retries", 0)
+
+        question = ev.input
+
+        cypher_query = await generate_cypher_step(
+            self.llm, question, self.few_shot_retriever
+        )
+
+        ctx.write_event_to_stream(
+            SseEvent(
+                message=f"Generated Cypher: {cypher_query}", label="Cypher generation"
+            )
+        )
+
+        # Return for the next step
+        return ExecuteCypherEvent(question=question, cypher=cypher_query)
+
+    @step
+    async def execute_query(
+        self, ctx: Context, ev: ExecuteCypherEvent
+    ) -> EvaluateEvent | CorrectCypherEvent:
+        # Get global var
+        retries = await ctx.get("retries")
+        try:
+            # Hard limit to 100 records
+            database_output = str(graph_store.structured_query(ev.cypher)[:100])
+        except Exception as e:
+            database_output = str(e)
+            # Retry
+            if retries < self.max_retries:
+                await ctx.set("retries", retries + 1)
+                return CorrectCypherEvent(
+                    question=ev.question, cypher=ev.cypher, error=database_output
+                )
+
+        return EvaluateEvent(
+            question=ev.question, cypher=ev.cypher, context=database_output
+        )
+
+    @step
+    async def evaluate_context(
+        self, ctx: Context, ev: EvaluateEvent
+    ) -> SummarizeEvent | CorrectCypherEvent:
+        # Get global var
+        retries = await ctx.get("retries")
+        evaluation = await evaluate_database_output_step(
+            self.llm, ev.question, ev.cypher, ev.context
+        )
+        if retries < self.max_retries and not evaluation == "Ok":
+            await ctx.set("retries", retries + 1)
+            return CorrectCypherEvent(
+                question=ev.question, cypher=ev.cypher, error=evaluation
+            )
+        return SummarizeEvent(
+            question=ev.question, cypher=ev.cypher, context=ev.context
+        )
+
+    @step
+    async def correct_cypher_step(
+        self, ctx: Context, ev: CorrectCypherEvent
+    ) -> ExecuteCypherEvent:
+        results = await correct_cypher_step(self.llm, ev.question, ev.cypher, ev.error)
+        return ExecuteCypherEvent(question=ev.question, cypher=results)
+
+    @step
+    async def summarize_answer(self, ctx: Context, ev: SummarizeEvent) -> StopEvent:
+        naive_final_answer_prompt = get_naive_final_answer_prompt()
+        gen = await self.llm.astream_chat(
+            naive_final_answer_prompt.format_messages(
+                context=ev.context, question=ev.question, cypher_query=ev.cypher
+            )
+        )
+        final_answer = ""
+        async for response in gen:
+            final_answer += response.delta
+            ctx.write_event_to_stream(
+                SseEvent(message=response.delta, label="Final answer")
+            )
+
+        stop_event = StopEvent(
+            result={
+                "cypher": ev.cypher,
+                "question": ev.question,
+                "answer": final_answer,
+            }
+        )
+
+        # Return the final result
+        return stop_event
