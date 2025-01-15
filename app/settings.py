db85ee52d0a8ecece779adb8f8aa8505e65ec10f
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2025-01-15 13:55:22 +0100

    change default

--- app/settings.py
+++ app/settings.py
@@ -4,8 +4,8 @@ from app.workflows.naive_text2cypher_retry import NaiveText2CypherRetryFlow
 from app.workflows.text2cypher_retry_check import NaiveText2CypherRetryCheckFlow
 
 WORKFLOW_MAP = {
+    "text2cypher with 1 retry and output check": NaiveText2CypherRetryCheckFlow,
     "naive_text2cypher": NaiveText2CypherFlow,
     "naive text2cypher with 1 retry": NaiveText2CypherRetryFlow,
-    "text2cypher with 1 retry and output check": NaiveText2CypherRetryCheckFlow,
     "iterative planning": IterativePlanningFlow,
 }
